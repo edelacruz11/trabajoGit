074a469f645154e3c6cd42124dadc3899a237eb7
eloy <edelacruz@elpuig.xeill.net>
2024-04-10 19:37:26 +0200

    Metodos agregados

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+
+    private static void method1() {
+        System.out.printf("Hola soy el metodo uno jejeje");
+    }
+
+    private static void method2() {
+
+    }
+
+    private static void method3() {
+
+    }
+
+    private static void method4() {
+
+    }
+}
